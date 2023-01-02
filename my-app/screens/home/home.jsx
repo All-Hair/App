37d7ac4aec3316d03cff64c390ab5b343d4be658
Saif-Ayari <saifayari312@gmail.com>
2023-01-02 19:41:12 +0100

    curved bottom

--- my-app/screens/home/home.jsx
+++ my-app/screens/home/home.jsx
@@ -1,51 +1,47 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { auth } from '../../firebase';
-// import  Navbar from '../../components/Navbar';
+import  Navbar from '../../components/Navbar';
+// import { TabBar } from '../../components/Curved-Bottom-bar/TabBar';
 
-
-
-const Home = ({navigation} ) => {
-
+const Home = ({ navigation }) => {
   const handleSignOut = () => {
     auth
       .signOut()
       .then(() => {
-        navigation.replace("Login")
+        navigation.replace('Login');
       })
-      .catch(error => alert(error.message))
-  }
-console.log(auth.currentUser?.email);
+      .catch((error) => alert(error.message));
+  };
+  console.log(auth.currentUser?.email);
   return (
-   
-   <View style={styles.container}>
+    <View style={styles.container}>
       <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
       <TouchableOpacity
-        onPress={handleSignOut}
+        onPress={() => {
+          navigation.navigate('Navbar');
+        }}
         style={styles.button}
       >
         <Text style={styles.buttonText}>Sign out</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={()=>{ navigation.navigate("Navbar")}}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
-      
+        <Navbar />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-   button: {
+  button: {
     backgroundColor: '#0782F9',
     width: '60%',
     padding: 15,
@@ -58,4 +54,4 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 16,
   },
-})
+});
--- my-app/screens/login/field.jsx
+++ my-app/screens/login/field.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import {TextInput} from 'react-native'
-import DropShadow from "react-native-drop-shadow";
-import {black} from './constants';
+import { TextInput } from 'react-native';
+// import DropShadow from "react-native-drop-shadow";
+import { black } from './constants';
 
-const Field = props => {
+const Field = (props) => {
   return (
     <TextInput
       {...props}
-      style={{borderRadius: 10, color: black,  paddingHorizontal: 18, width: '70%', backgroundColor: 'white',borderColor:'#CCC9C0',borderWidth:1,shadowColor:'#CCC9C0' , marginVertical: 30 , height:50}}
-      ></TextInput>
+      style={{
+        borderRadius: 10,
+        color: black,
+        paddingHorizontal: 18,
+        width: '70%',
+        backgroundColor: 'white',
+        borderColor: '#CCC9C0',
+        borderWidth: 1,
+        marginVertical: 30,
+        height: 70,shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 6,
+        },
+        shadowOpacity: 0.37,
+        shadowRadius: 7.49,
+        
+        elevation: 12,
+      }}
+    ></TextInput>
   );
 };
 
