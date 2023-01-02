88d72a92ccd929644b74c9f8682ab36152c47354
ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-01-02 18:33:05 +0100

    add styele to Onepost componet

--- my-app/screens/shop/OneShop.jsx
+++ my-app/screens/shop/OneShop.jsx
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const OneShop = () => {
   return (
-    <View>
-      <Text>OneShop</Text>
+    <View style={{ alignItems: "center"}}>
+      <Text  style={{
+            // color: "#000000",
+            fontSize: 35,
+            fontWeight: "bold",
+            marginVertical: 10,
+            marginTop: 20 ,
+            paddingBottom: 50,
+            alignItems: "center"
+            }}>DETAIL PRODUCT </Text>
+            <Image
+            style={{width: '70%', height: '50%'}}
+            source={{uri:'https://engineering.fb.com/wp-content/uploads/2016/04/yearinreview.jpg'}}
+               />
+               <Text style={{fontSize: 35,}}>Product description</Text>
+               <Text  style={{fontSize: 20,}}>iwhibsibiubiubsuibiusuibuhicsoIO</Text>
+               <Button title=''/>
     </View>
   )
 }
