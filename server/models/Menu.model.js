668603457bad410d2c6a8bb82b2bb875780c8ed1
ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-01-04 00:17:07 +0100

    add Menu model in server

---
+++ server/models/Menu.model.js
@@ -0,0 +1,20 @@
+module.exports=(sequelize,DataType)=>{
+    const Menu = sequelize.define('Menu',{
+        service:{
+            type:DataType.STRING,
+            allowNull:false
+        },
+        price:{
+            type:DataType.STRING,
+            allowNull:false
+        },
+        image:{
+            type:DataType.STRING,
+            allowNull:false
+        },
+        category:{
+            type:DataType.STRING,
+            allowNull:false
+        }
+    })
+}
