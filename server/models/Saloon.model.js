296d81ebf09c56b2d7c583ed934266e30f50de26
ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-01-03 22:58:08 +0100

    add Saloon model to server

---
+++ server/models/Saloon.model.js
@@ -0,0 +1,49 @@
+module.exports=(sequelize,Datatype)=>{
+ const Saloon = sequelize.define("Saloon",{
+    name:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    adress:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    state:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    city:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    type:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    image:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    gender:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    description:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    email:{
+        type:Datatype.STRING,
+        allowNull:false
+    },
+    home_service:{
+        type:Datatype.BOOLEAN,
+        defaultValue : false 
+    },
+    rate:{
+        type:Datatype.TINYINT,
+        allowNull:false
+    }
+    
+ })
+}
