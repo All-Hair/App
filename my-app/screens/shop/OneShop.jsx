69bdaaa0992d9dfe6ef5f8678da23b453c083e8c
ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-01-03 14:24:40 +0100

    fix style

--- my-app/screens/shop/OneShop.jsx
+++ my-app/screens/shop/OneShop.jsx
@@ -3,29 +3,65 @@ import React from 'react'
 
 const OneShop = ({navigation}) => {
   return (
-    <View style={{ alignItems: "center"}}>
-      <Text  style={{
-            // color: "#000000",
-            fontSize: 35,
-            fontWeight: "bold",
-            marginVertical: 10,
-            marginTop: 20 ,
-            paddingBottom: 50,
-            alignItems: "center"
-            }}>DETAIL PRODUCT </Text>
+    <View>
+      <Text  style={{ alignSelf: 'center',fontSize: 35,}}>DETAIL PRODUCT </Text>
+    <View style={styles.container}>
             <Image
-            style={{width: '70%', height: '50%'}}
+           style={styles.avatar}
             source={{uri:'https://engineering.fb.com/wp-content/uploads/2016/04/yearinreview.jpg'}}
                />
                <Text style={{fontSize: 35,}}>Product description</Text>
                <Text  style={{fontSize: 20,}}>iwhibsibiubiubsuibiusuibuhicsoIO</Text>
-               <Button title='Buy 'onPress={()=>{navigation.navigate('cart')}}/>
-
-              
+    </View>
+               <Button style={styles.button} title='Buy 'onPress={()=>{navigation.navigate('cart')}}/>
     </View>
   )
 }
 
 export default OneShop
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: '#0891b2',
+    paddingVertical: 50,
+    paddingHorizontal: 120,
+    borderRadius: 50,
+    alignSelf: 'center',
+    width: 5000,
+    maxWidth: '100%'
+  },
+  timings: {
+    color: '#fff',
+    fontSize: '14px'
+  },
+  metaContainer: {
+    justifyContent: 'space-between'
+  },
+  topContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  avatar: {
+    height: 200,
+    width: 200,
+    // borderRadius: 100
+  },
+  description: {
+    color: 'white',
+    marginTop: 5,
+    fontSize: 20
+  },
+  button: {
+    backgroundColor: '#22d3ee',
+    alignSelf: 'flex-start',
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 2
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'white',
+    textTransform: 'uppercase',
+    fontSize: 14
+  }
+});
