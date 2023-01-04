f76b535020f7a05cf6472667c35a543503992302
Saif-Ayari <saifayari312@gmail.com>
2023-01-04 18:57:38 +0100

    home page

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -1,196 +1,224 @@
-import { View, Image, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Animated, Text, Alert,Dimensions} from 'react-native';
+import {
+  Alert,
+  Animated,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
-import React from 'react'
+import React from 'react';
 const { width, height } = Dimensions.get('window');
 
+const Navbar = ({ navigation }) => {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        top: height - 70,
+        flexDirection: 'column',
+        width: width,
+        position: 'absolute',
+      }}
+    >
+      <View
+        style={{
+          alignSelf: 'center',
+          backgroundColor: 'grey',
+          width: width / 6.5,
+          height: width / 6.5,
+          borderRadius: 35,
+          bottom: 35,
+          zIndex: 10,
+        }}
+      >
+        <TouchableOpacity>
+          <View style={[styles.button, styles.actionBtn]}>
+            <Image
+              style={{ width: 50, height: 50 }}
+              resizeMode="contain"
+              source={require('my-app/assets/logo-removebg-preview.png')}
+              onPress={() => {
+                Alert.alert('ok');
+              }}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          backgroundColor: '#CCC9C0',
+          border: 2,
+          radius: 3,
+          shadowOpacity: 0.3,
+          shadowRadius: 3,
+          shadowOffset: {
+            height: 3,
+            width: 3,
+          },
+          x: 0,
+          y: 0,
+          style: { marginVertical: 5 },
+          bottom: 0,
+          width: '100%',
+          height: 70,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingVertical: 10,
+          paddingHorizontal: 25,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              Alert.alert('click');
+            }}
+          >
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={{
+                uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/6178238-removebg-preview_cpuyde.png',
+              }}
+              onPress={() => {
+                Alert.alert('');
+              }}
+            ></Image>
+          </TouchableOpacity>
+          <Text style={{ justifyContent: 'center', alignItems: 'center' }}>
+            Home
+          </Text>
+        </View>
 
-const Navbar = ({navigation}) => {
-    
+        <View
+          style={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginStart: 30,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              Alert.alert('click');
+            }}
+          >
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={{
+                uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/5043688-removebg-preview_v3u4hm.png',
+              }}
+              onPress={() => {
+                Alert.alert('click');
+              }}
+            />
+          </TouchableOpacity>
+          <Text style={{ justifyContent: 'center', alignItems: 'center' }}>
+            search{' '}
+          </Text>
+        </View>
 
+        <View
+          style={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginStart: 85,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Shop');
+            }}
+          >
+            <Image
+              source={{
+                uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/2838838-removebg-preview_xksvd2.png',
+              }}
+              onPress={() => {
+                navigation.navigate('shop');
+              }}
+              style={{ marginHorizontal: 16, width: 30, height: 30 }}
+              containerStyle={{ marginHorizontal: 16 }}
+            />
+          </TouchableOpacity>
+          <Text style={{ justifyContent: 'center', alignItems: 'center' }}>
+            Shop
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Sprofile');
+            }}
+          >
+            <Image
+              source={{
+                uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/3661442-removebg-preview_cwdejl.png',
+              }}
+              style={{ marginHorizontal: 16, width: 30, height: 30 }}
+              containerStyle={{ marginHorizontal: 16 }}
+            />
+          </TouchableOpacity>
+          <Text style={{ justifyContent: 'center', alignItems: 'center' }}>
+            {' '}
+            Profile{' '}
+          </Text>
+        </View>
 
-   
-        return (
-            <View style={{
-                flex: 1,
-                top:height -70,
-                flexDirection: 'column',
-                width: width,                
-                position: 'absolute',
-            }}>
-
-                 <View style={{
-
-                   
-                    alignSelf: 'center',
-                    backgroundColor: 'grey',
-                    width: 70,
-                    height: 70,
-                    borderRadius: 35,
-                    bottom: 35,
-                    zIndex: 10
-
-
-                }}>
-
-                    <TouchableOpacity >
-                        <View style={[styles.button, styles.actionBtn]}>
-                           
-                            <Image style={{ width: 50, height: 50 }}
-                                resizeMode="contain"
-                                source={ require('my-app/assets/logo-removebg-preview.png')}  onPress={()=>{Alert.alert("ok")}}/>
-                        </View>
-                    </TouchableOpacity>
-                </View> 
-                <View style={{
-
-                    position: 'absolute',
-                    backgroundColor: '#CCC9C0',
-                    border: 2,
-                    radius: 3,
-                    shadowOpacity: 0.3,
-                    shadowRadius: 3,
-                    shadowOffset: {
-
-                        height: 3, width: 3
-                    },
-                    x: 0,
-                    y: 0,
-                    style: { marginVertical: 5 },
-                    bottom: 0,
-                    width: '100%',
-                    height: 70,
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    paddingVertical: 10,
-                    paddingHorizontal: 25
-
-
-                }}>
-
-                    <View style={{
-
-
-                        flexDirection: 'column', alignItems: 'center', justifyContent: 'center'
-                    }}>
-                        <TouchableOpacity onPress={() => { Alert.alert('click') }}>
-                            <Image
-
-                                style={{ width: 30, height: 30 }}
-
-                                source={{ uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/6178238-removebg-preview_cpuyde.png' }}
-
-                                onPress={()=>{Alert.alert("")}}
-                            >
-
-                            </Image>
-
-                        </TouchableOpacity>
-                        <Text style={{justifyContent:'center',alignItems:'center'}}>Home</Text>
-                    </View>
-                    
-                    <View style={{
-                        flexDirection: 'column', alignItems: 'center',justifyContent:'center',marginStart:30
-                    }}>
-
-                        <TouchableOpacity
-                            onPress={() => { Alert.alert("click") }}
-                        >
-                            <Image
-                                style={{  width: 30, height: 30 }}
-                                source={{ uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/5043688-removebg-preview_v3u4hm.png' }}
-                                onPress={() => { Alert.alert("click") }}
-                            />
-                       
-                        </TouchableOpacity>
-                        <Text style={{justifyContent:'center',alignItems:'center' }}>search </Text>
-                    </View>
-
-                        <View style={{
-                             flexDirection: 'column', alignItems: 'center',justifyContent:'space-between',marginStart:85,
-                        }}>
-
-                            <TouchableOpacity
-                             onPress={() => { navigation.navigate('Shop') }}
-
-                            >
-                                <Image
-                                    source={{ uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/2838838-removebg-preview_xksvd2.png' }}
-                                    onPress={() => { navigation.navigate('shop') }}
-                                    style={{ marginHorizontal: 16, width: 30, height: 30 }}
-                                    containerStyle={{ marginHorizontal: 16 }}
-                                />
-                       
-                            </TouchableOpacity>
-                            <Text style={{justifyContent:'center',alignItems:'center' }}>Shop</Text>
-                        </View>
-                        <View style={{
-                            flexDirection: 'column', alignItems: 'center',justifyContent:'flex-end',
-                          
-                        }}>
-                            <TouchableOpacity
-                                onPress={() => {navigation.navigate("Sprofile")}}
-                            >
-                                <Image
-                                    source={{ uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/3661442-removebg-preview_cwdejl.png' }}
-
-                                    
-                                    style={{ marginHorizontal: 16, width: 30, height: 30 }}
-                                    containerStyle={{ marginHorizontal: 16 }}
-                                />
-                     
-                            </TouchableOpacity>
-                            <Text style={{justifyContent:'center',alignItems:'center' }}> Profile </Text>
-                           
-                        </View>
-
-                    {/* </View> */}
-                </View>
-            </View>
-        );
-    
-
-    
-}
-
+        {/* </View> */}
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    MainContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#CCC9C0'
-    },
-    button: {
-        width: 60,
-        height: 60,
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: 'grey',
-        shadowOpacity: 0.1,
-        shadowOffset: { x: 2, y: 0 },
-        shadowRadius: 2,
-        borderRadius: 30,
-        position: 'absolute',
-        bottom: 20,
-        right: 0,
-        top: 5,
-        left: 5,
-        shadowOpacity: 6.0,
-
-    },
-    actionBtn: {
-
-        backgroundColor: '#CCC9C0',
-        textShadowOffset: { width: 5, height: 5 },
-        textShadowRadius: 10,
-        borderWidth: 2,
-        borderColor: '#fff'
-        
-
-    }
-
-
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CCC9C0',
+  },
+  button: {
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: 'grey',
+    shadowOpacity: 0.1,
+    shadowOffset: { x: 2, y: 0 },
+    shadowRadius: 1,
+    borderRadius: 35,
+    position: 'absolute',
+    // bottom: -70,
+    right: 0,
+    top: 4,
+    left: 4.5,
+    shadowOpacity: 6.0,
+  },
+  actionBtn: {
+    backgroundColor: '#CCC9C0',
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 10,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
 });
 
-export default Navbar
+export default Navbar;
--- my-app/screens/home/home.jsx
+++ my-app/screens/home/home.jsx
@@ -1,20 +1,27 @@
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { auth } from '../../firebase';
-// import Shop from '../shop/shop'
 import { NavigationContainer } from '@react-navigation/native';
-// import  Navbar from '../../components/Navbar';
-import  Navbar from '../../components/Navbar';
+import React ,{useState}from 'react';
+import {
+  Button,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { auth } from '../../firebase';
 
+import Animated, { Easing } from 'react-native-reanimated';
+import Navbar from '../../components/Navbar';
 
-
-
-
-
-
-
+const { Value, timing } = Animated;
 
 const Home = ({ navigation }) => {
+  // const [_scroll_y,setScroll_y]=useState(0)
+  const _scroll_y = new Value(0)
   const handleSignOut = () => {
     auth
       .signOut()
@@ -25,42 +32,129 @@ const Home = ({ navigation }) => {
   };
   console.log(auth.currentUser?.email);
   return (
-  
-   <View style={styles.container}>
-   
-      <Text>Email: {auth.currentUser?.email}</Text>
+    <SafeAreaView style={styles.container}>
+      <Animated.View style={styles.header}>
+      <TextInput
+        style={{
+          borderRadius: 10,
+          color: 'black',
+          paddingHorizontal: 18,
+          width: width - 120,
+          backgroundColor: '#D9D9D9',
+          borderColor: '#CCC9C0',
+          borderWidth: 1,
+          marginTop: 35,
+          height: 40,
+          marginLeft:width -390
+        }}
+        placeholder="search"
+      ></TextInput>
+      <Image
+        source={require('my-app/assets/logo-removebg-preview.png')}
+        style={{ width: 170, height: 170, marginTop: 30 ,marginLeft:width-320 }}
+      />
+      <TouchableOpacity style={{ marginLeft:50 }}>
+        <View>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={require('my-app/assets/noti.png')}
+          />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={{  marginLeft:width-350,top:10 }}>
+        <View>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={{
+              uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/5043688-removebg-preview_v3u4hm.png',
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ marginLeft: width - 245 }}>
+        <View>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={{
+              uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/6178238-removebg-preview_cpuyde.png',
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ marginLeft: width -160,bottom:67}}>
+        <View>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={{
+              uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/2838838-removebg-preview_xksvd2.png',
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ marginLeft: width - 100 ,bottom:152}}>
+        <View>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={{
+              uri: 'https://res.cloudinary.com/dxvvdq91a/image/upload/v1672691231/3661442-removebg-preview_cwdejl.png',
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+      <View style={styles.fake_icon_box}>
+
+      </View>
+      </Animated.View>
+      <Animated.ScrollView style={[styles.scroll_view ,{}]} showsVerticalScrollIndicator={false}
+      bounces={false}
+      scrollEventThrottle={5}
+      onScroll={Animated.event([{
+        nativeEvent:{contentOffset:{y:_scroll_y}}
+      }])}>
+        <View style={[styles.fake_post,{backgroundColor:'#222222'}]}></View>
+        <View style={[styles.fake_post,{backgroundColor:'#333333'}]}></View>
+        <View style={[styles.fake_post,{backgroundColor:'#444444'}]}></View>
+        <View style={[styles.fake_post,{backgroundColor:'#555555'}]}></View>
+        <View style={[styles.fake_post,{backgroundColor:'#666666'}]}></View>
+        
+
+      </Animated.ScrollView>
+
+      {/* <Text>Email:{auth.currentUser?.name}</Text>
       <TouchableOpacity onPress={handleSignOut} style={styles.button}>
         <Text style={styles.buttonText}>Sign out</Text>
       </TouchableOpacity>
       <TouchableOpacity
-
-        onPress={()=>{ navigation.navigate("Sprofile")}}
-
+        onPress={() => {
+          navigation.navigate('Sprofile');
+        }}
         style={styles.button}
       >
-        <Text style={styles.buttonText}>Sprofile</Text>
-     
+        <Text style={styles.buttonText}>Profile</Text>
       </TouchableOpacity>
-      
-      <Button title="Go to Shop" onPress={()=>navigation.navigate("Shop")}/>
-        <Navbar />
 
-      
-        <Navbar  navigation ={navigation}/>
-    </View>
+        
+      <TouchableOpacity style={styles.button}  onPress={() => navigation.navigate('Shop')} >
+        <Text>go ot the hell</Text>
+        </TouchableOpacity>
+      <Navbar /> */}
+
+      {/* <Navbar navigation={navigation} /> */}
+    </SafeAreaView>
   );
 };
 
 export default Home;
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignItems: 'center',
   },
   button: {
-    backgroundColor: '#0782F9',
+    backgroundColor: '#CCC9C0',
     width: '60%',
     padding: 15,
     borderRadius: 10,
@@ -72,4 +166,34 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 16,
   },
+  header: {
+    position: 'absolute',
+    width: width,
+    height: height - 390,
+    backgroundColor: '#CCC9C0',
+    borderBottomLeftRadius: 1000,
+    borderBottomRightRadius: 1000,
+  },
+  circleButton: {
+    width: 50,
+    height: 50,
+  },
+  scroll_view:{
+    flex:1  
+  },
+  fake_post:{
+    height:250,
+    marginHorizontal:16,
+    marginTop:16,
+    borderRadius:8 
+  },
+  fake_icon_box:{
+    backgroundColor:'#e4e6eb',
+    width:40,
+    height:40,
+    borderRadius:40,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
