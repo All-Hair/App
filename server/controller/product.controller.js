b699b9c125ac3cbccd1718d1fcb6da633a172adf
Zied-Bitri <zied.bitri@gmail.com>
2023-01-05 11:32:33 +0100

    merge

---
+++ server/controller/product.controller.js
@@ -0,0 +1,4 @@
+const db = require("../models/Product.model");
+const Product = db.Product;
+const Op = db.Sequelize.Op;
+
--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const cors = require("cors");
 const db =require('./models')
 const app = express()  
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+app.use(cors());
 
 
 app.get('/', (req, res) => {
     res.send('welcome in All-hair')
   })
+
+  require("./app/routes/product")(app);
 
 
 const PORT = 5000; 
