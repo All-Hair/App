d8c3be3c987f24de461939a877a0fc10ba4a229d
ahmed henchiri <ahmedhenchiri00@gmail.com>
2023-01-08 13:26:42 +0100

    fix slider in frontend

--- my-app/screens/shop/Slider.jsx
+++ my-app/screens/shop/Slider.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
-import { View,  Text, StyleSheet ,Image, SafeAreaView, TouchableHighlight} from 'react-native';
+import { View,  Text, StyleSheet ,Image, SafeAreaView, TouchableHighlight, Dimensions} from 'react-native';
 import ImageSlider from 'react-native-image-slider';
 // import { Image } from 'react-native-svg';
 import Slider from '@react-native-community/slider';
+import { ScrollView } from 'native-base';
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    paddingTop: 50,
+   flex:1,
+  },
+  wrap:{
+  width:WIDTH,
+  height:HEIGTH * 0.25
+  },
+  wrapDot:{
+  position:'absolute',
+  bottom:0,
+  flexDirection:'row',
+  alignSelf:"center"
+  },
+  dotActive:{
+   margin:3,
+   color:"black"
+  },
+  dot:{
+  margin:3,
+  color:'white'
   },
   image: {
     width: 200,
@@ -26,54 +44,52 @@ const styles = StyleSheet.create({
 
 const SliderPhotos = () => {
 //   const [currentIndex, setCurrentIndex] = useState(0);
+const [imgActive,setimgActive] = useState(0)
+
+onchange=(nativeEvent)=>{
+  if(nativeEvent){
+    const slide=Math.ceil(nativeEvent.contentOffset.x /nativeEvent.layoutMeasurement.width);
+    if(slide != imgActive){
+      setimgActive(slide)
+    }
+  }
+}
   const images = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-7hByhjV3B_CWuKlyQ3OmY7bNyqfInaatww&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD_qb00E2YueF-BC5-CX3nzZLwoaEZfJ_8fw&usqp=CAU',
     'https://m.media-amazon.com/images/I/61N6De+CSdL.jpg',
   ];
-
-  const handleSlide = (index) => {
-    setCurrentIndex(index);
-  }
+const WIDTH =Dimensions.get('window').width
+const HEIGTH = Dimensions.get('window').height
 
   return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.content1}>
-          <Text style={styles.contentText}>Content 1</Text>
-        </View>
-        <ImageSlider
-          loopBothSides
-          autoPlayWithInterval={3000}
-          images={images}
-          customSlide={({ index, item, style, width }) => (
-            // It's important to put style here because it's got offset inside
-            <View key={index} style={[style, styles.customSlide]}>
-              <Image source={{ uri: item }} style={styles.customImage} />
-            </View>
-          )}
-          customButtons={(position, move) => (
-            <View style={styles.buttons}>
-              {images.map((image, index) => {
-                return (
-                  <TouchableHighlight
-                    key={index}
-                    underlayColor="#ccc"
-                    onPress={() => move(index)}
-                    style={styles.button}
-                  >
-                    <Text style={position === index && styles.buttonSelected}>
-                      {index + 1}
-                    </Text>
-                  </TouchableHighlight>
-                );
-              })}
-            </View>
-          )}
-        />
-        <View style={styles.content2}>
-          <Text style={styles.contentText}>Content 2</Text>
-        </View>
-      </SafeAreaView>
+   <SafeAreaView style={styles.container}>
+   <View style={styles.wrap}>
+   <ScrollView onScroll={({nativeEvent}) => onchange(nativeEvent)}
+   showsHorizontalScrollIndicator={false}
+   pagingEnabled
+   horizontal
+   style={styles.wrap}
+   >
+  {images.map((e,i)=>
+  <Image key={i} 
+  resizeMode="stretch"
+  style={styles.wrap}
+  source={{uri:e}}
+  />
+  )}
+   </ScrollView>
+   <View style={styles.wrapDot}>
+  {images.map((e,index)=>
+  <Text key={e} 
+  style={imgActive == index ? styles.dotAcitve :styles.dot}
+  >
+    ●
+  </Text>
+  )}
+   </View>
+   </View>
+   </SafeAreaView>
   );
 }
 export default SliderPhotos
